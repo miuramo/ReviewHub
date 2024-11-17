06ab28608f09ba9347309f107a37d2d071c029d3
motoki miura <miura@moto.qee.jp>
2024-11-17 18:29:36 +0900

    d

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -3,12 +3,6 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-2 lg:px-4">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <!-- Logo -->
-                {{-- <div class="shrink-0 flex items-center">
-                    <a href="/" title="トップページへのリンク" class="text-bold text-2xl hover:text-blue-700">
-
-                    </a>
-                </div> --}}
 
                 <div class="hidden space-x-8 sm:-my-px sm:ms-5 sm:flex">
                     <x-nav-link :href="route('guesttop')" :active="request()->routeIs('guesttop')" size="2xl" weight="bold">
