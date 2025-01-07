4db8755ae114f261439b93b80ee804095b4ed5ed
motoki miura <miura@moto.qee.jp>
2025-01-07 19:52:00 +0900

    seeder

--- database/seeders/EnqueteAnswerSeeder.php
+++ database/seeders/EnqueteAnswerSeeder.php
@@ -12,50 +12,52 @@ class EnqueteAnswerSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\EnqueteAnswer::factory()->create([
-            'enquete_id' => 1,
-            'enquete_item_id' => 1,
-            'user_id' => 1,
-            'paper_id' => 1,
-            'value' => null,
-            'valuestr' => "そうぞう たろう",
-        ]);
+        if (env('APP_DEBUG')) {
+            \App\Models\EnqueteAnswer::factory()->create([
+                'enquete_id' => 1,
+                'enquete_item_id' => 1,
+                'user_id' => 1,
+                'paper_id' => 1,
+                'value' => null,
+                'valuestr' => "そうぞう たろう",
+            ]);
 
-        \App\Models\EnqueteAnswer::factory()->create([
-            'enquete_id' => 1,
-            'enquete_item_id' => 2,
-            'user_id' => 1,
-            'paper_id' => 1,
-            'value' => null,
-            'valuestr' => "03-1234-5678",
-        ]);
+            \App\Models\EnqueteAnswer::factory()->create([
+                'enquete_id' => 1,
+                'enquete_item_id' => 2,
+                'user_id' => 1,
+                'paper_id' => 1,
+                'value' => null,
+                'valuestr' => "03-1234-5678",
+            ]);
 
-        \App\Models\EnqueteAnswer::factory()->create([
-            'enquete_id' => 1,
-            'enquete_item_id' => 3,
-            'user_id' => 1,
-            'paper_id' => 1,
-            'value' => null,
-            'valuestr' => "111-2222 東京都千代田区1-2",
-        ]);
+            \App\Models\EnqueteAnswer::factory()->create([
+                'enquete_id' => 1,
+                'enquete_item_id' => 3,
+                'user_id' => 1,
+                'paper_id' => 1,
+                'value' => null,
+                'valuestr' => "111-2222 東京都千代田区1-2",
+            ]);
 
-        \App\Models\EnqueteAnswer::factory()->create([
-            'enquete_id' => 1,
-            'enquete_item_id' => 4,
-            'user_id' => 1,
-            'paper_id' => 1,
-            'value' => 800,
-            'valuestr' => "800",
-        ]);
+            \App\Models\EnqueteAnswer::factory()->create([
+                'enquete_id' => 1,
+                'enquete_item_id' => 4,
+                'user_id' => 1,
+                'paper_id' => 1,
+                'value' => 800,
+                'valuestr' => "800",
+            ]);
 
-        \App\Models\EnqueteAnswer::factory()->create([
-            'enquete_id' => 1,
-            'enquete_item_id' => 5,
-            'user_id' => 1,
-            'paper_id' => 1,
-            'value' => 8,
-            'valuestr' => "8",
-        ]);
+            \App\Models\EnqueteAnswer::factory()->create([
+                'enquete_id' => 1,
+                'enquete_item_id' => 5,
+                'user_id' => 1,
+                'paper_id' => 1,
+                'value' => 8,
+                'valuestr' => "8",
+            ]);
+        }
 
         //
     }
