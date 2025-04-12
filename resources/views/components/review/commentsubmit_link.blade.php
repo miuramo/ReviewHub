690a4fe0da625f302e3c43eb84ab082c10b32b33
motoki miura <miura@moto.qee.jp>
2025-04-12 16:07:47 +0900

    remove _blank

--- resources/views/components/review/commentsubmit_link.blade.php
+++ resources/views/components/review/commentsubmit_link.blade.php
@@ -9,11 +9,11 @@
 @endphp
 <!-- components.review.commentpaper_link  -->
 @if($color)
-    <x-element.linkbutton target="_blank" href="{{ route('review.commentsubmit', ['sub'=>$sub, 'token' => $sub->token() ]) }}" color="{{ $color }}">
+    <x-element.linkbutton href="{{ route('review.commentsubmit', ['sub'=>$sub, 'token' => $sub->token() ]) }}" color="{{ $color }}">
         {{ $label }}
     </x-element.linkbutton>
 @else
-<a class="hover:underline" href="{{ route('review.commentsubmit', ['sub'=>$sub, 'token' => $sub->token() ]) }}" target="_blank">
+<a class="hover:underline" href="{{ route('review.commentsubmit', ['sub'=>$sub, 'token' => $sub->token() ]) }}" >
     {{ $label }}
 </a>
 @endif
