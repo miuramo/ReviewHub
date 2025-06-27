aa937617640b64eb015ebbe60875a9c857294fa8
motoki miura <miura@moto.qee.jp>
2025-06-27 11:40:16 +0900

    delete button

--- resources/views/components/element/deletebutton.blade.php
+++ resources/views/components/element/deletebutton.blade.php
@@ -6,10 +6,15 @@
     'size' => 'md',
 ])
 <!-- components.element.deletebutton -->
+@if (strlen($action) < 2)
+    <div class="text-red-500 bg-yellow-200 font-bold">注意: deletebuttonコンポーネントは、href属性を使用しません。代わりに、action属性を使用してください。
+    </div>
+@endif
 <form action="{{ $action }}" method="post" class="inline-block float-{{ $align }}">
     @method('DELETE')
     @csrf
-    <button type="submit" onclick="return confirm('{{ $confirm }}')" class="inline-flex justify-center py-1 px-2 border border-transparent shadow-md text-{{$size}} font-medium rounded-md text-white bg-{{$color}}-500 hover:bg-{{$color}}-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-{{$color}}-500">
+    <button type="submit" onclick="return confirm('{{ $confirm }}')"
+        class="inline-flex justify-center py-1 px-2 border border-transparent shadow-md text-{{ $size }} font-medium rounded-md text-white bg-{{ $color }}-500 hover:bg-{{ $color }}-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-{{ $color }}-500">
         {{ $slot }}
     </button>
 </form>
