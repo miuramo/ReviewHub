6e9063b314e6ad797ebba2459671055ea350ab3d
motoki miura <miura@moto.qee.jp>
2025-11-27 12:34:55 +0900

    d

--- resources/views/bb/index_for_pub.blade.php
+++ resources/views/bb/index_for_pub.blade.php
@@ -65,8 +65,8 @@
             @endforeach
         </div>
         <div class="m-6">
-            <x-element.linkbutton href="{{ route('bb.multisubmit', ['type' => 3]) }}" color="teal" size="md">
-                出版掲示板への一括書き込み
+            <x-element.linkbutton href="{{ route('bb.multisubmit') }}" color="pink" size="md">
+                著者掲示板への一括書き込み
             </x-element.linkbutton>
         </div>
 
