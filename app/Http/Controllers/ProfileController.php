90ed73aadbb9e4090ceee737eec5a1eb1add1441
motoki miura <miura@moto.qee.jp>
2026-07-06 11:04:47 +0900

    accept multiple passkeys

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -19,6 +19,7 @@ class ProfileController extends Controller
     {
         return view('profile.edit', [
             'user' => $request->user(),
+            'passkeys' => $request->user()->passkeys()->latest()->get(),
         ]);
     }
 
